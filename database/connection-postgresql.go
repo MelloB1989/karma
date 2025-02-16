bd65320fad24071b0c7439f69bc801ad9eaad1b2
Kartik Deshmukh <kartikdd90@gmail.com>
2025-02-16 19:03:42 +0530

    Added db url parsing, instead of multiple env vars

--- database/connection-postgresql.go
+++ database/connection-postgresql.go
@@ -3,6 +3,8 @@ package database
 import (
 	"fmt"
 	"log"
+	"net/url"
+	"strings"
 
 	"github.com/MelloB1989/karma/config"
 	"github.com/jmoiron/sqlx"
@@ -16,11 +18,41 @@ func PostgresConn() (*sqlx.DB, error) {
 	var driverName string
 	var driverSource string
 
+	// Parse the URL
+	parsedURL, err := url.Parse(env.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Extract user info
+	userInfo := parsedURL.User
+	username := userInfo.Username()
+	password, _ := userInfo.Password()
+
+	// Extract host and port
+	host := parsedURL.Hostname()
+	port := parsedURL.Port()
+	if port == "" {
+		port = "5432" // Default PostgreSQL port
+	}
+
+	// Extract database name
+	pathSegments := strings.Split(parsedURL.Path, "/")
+	databaseName := pathSegments[len(pathSegments)-1]
+
+	// Extract SSL mode
+	queryParams := parsedURL.Query()
+	sslMode := queryParams.Get("sslmode")
+	if sslMode == "require" {
+		sslMode = "true"
+	} else {
+		sslMode = "false"
+	}
 	driverName = "postgres"
 	if ssl == "true" {
-		driverSource = fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=require", env.DatabaseUser, env.DatabaseName, env.DatabasePassword, env.DatabaseHost, env.DatabasePort)
+		driverSource = fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=require", username, databaseName, password, host, port)
 	} else {
-		driverSource = fmt.Sprintf("user=%s dbname=%s sslmode=disable password=%s host=%s port=%s", env.DatabaseUser, env.DatabaseName, env.DatabasePassword, env.DatabaseHost, env.DatabasePort)
+		driverSource = fmt.Sprintf("user=%s dbname=%s password=%s host=%s port=%s sslmode=disable", username, databaseName, password, host, port)
 	}
 
 	db, err := sqlx.Connect(driverName, driverSource)
