579505b8a4803528104d5c444e730a21f4f19254
Kartik Deshmukh <kartikdd90@gmail.com>
2025-04-13 02:09:47 +0530

    Added debugging in APIr

--- apir/main.go
+++ apir/main.go
@@ -5,19 +5,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
 type APIClient struct {
-	BaseURL string
-	Headers map[string]string
+	BaseURL   string
+	Headers   map[string]string
+	DebugMode bool
 }
 
 func NewAPIClient(baseURL string, headers map[string]string) *APIClient {
 	return &APIClient{
-		BaseURL: baseURL,
-		Headers: headers,
+		BaseURL:   baseURL,
+		Headers:   headers,
+		DebugMode: false,
 	}
+}
+
+func (client *APIClient) SetDebugMode(debug bool) {
+	client.DebugMode = debug
 }
 
 func (client *APIClient) sendRequest(method, endpoint string, body any) ([]byte, error) {
@@ -41,6 +48,17 @@ func (client *APIClient) sendRequest(method, endpoint string, body any) ([]byte,
 		req.Header.Set(key, value)
 	}
 
+	if client.DebugMode {
+		log.Printf("Request Method: %s", method)
+		log.Printf("Request URL: %s", url)
+		if body != nil {
+			log.Printf("Request Body: %s", reqBody)
+		}
+		for key, value := range client.Headers {
+			log.Printf("Request Header: %s: %s", key, value)
+		}
+	}
+
 	clientHTTP := &http.Client{}
 	resp, err := clientHTTP.Do(req)
 	if err != nil {
@@ -51,6 +69,11 @@ func (client *APIClient) sendRequest(method, endpoint string, body any) ([]byte,
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
+	}
+
+	if client.DebugMode {
+		log.Printf("Response Status: %s", resp.Status)
+		log.Printf("Response Body: %s", respBody)
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
